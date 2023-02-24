3d2804af6f66c786162bdc9ea61f08dd9416aaef
Clu <ncom3000@gmail.com>
2023-02-24 18:17:12 +0200

    init

--- src/AllowCors.php
+++ src/AllowCors.php
@@ -14,9 +14,17 @@ final class AllowCors
      */
     public function handle($request, \Closure $next)
     {
-    return $next($request)
-        ->header('Access-Control-Allow-Origin', '*')
-        ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS')
-        ->header('Access-Control-Allow-Headers', 'Content-Type, Authorizations');
+        $headers = [
+            'Access-Control-Allow-Origin' => '*',
+            'Access-Control-Allow-Methods' => 'GET, POST, PUT, PATCH, DELETE, OPTIONS',
+            'Access-Control-Allow-Headers' => 'DNT, X-CustomHeader, Keep-Alive, User-Agent, X-Requested-With, If-Modified-Since, Cache-Control, Content-Type, Content-Range, Range, Authorization',
+        ];
+        foreach ($headers as $name => $value) {
+            if (!$request->hasHeader($name)) {
+                $next($request)->header($name, $value);
+            }
+        }
+
+        return $next($request);
     }
 }
