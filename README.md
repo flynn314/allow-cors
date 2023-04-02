9ffca2c721e40084de72b5102958fa7fa7a037a2
Clu <ncom3000@gmail.com>
2023-04-02 09:14:45 +0300

    Simplified CORS. + Readme update.

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 ## Experimental library
+
+```php
+# 1. add to Kernel.php
+'cors.allow' => AllowCors::class,
+
+# 2. add middleware to route
+Route::middleware(['cors.allow'])->group(function () {
+    Route::get('/my-api-call', 'Controller@index');
+});
+```
--- src/AllowCors.php
+++ src/AllowCors.php
@@ -5,26 +5,8 @@ use Illuminate\Http\Request;
 
 final class AllowCors
 {
-    /**
-     * Handle an incoming request
-     *
-     * @param  Request  $request
-     * @param  \Closure  $next
-     * @return mixed
-     */
-    public function handle($request, \Closure $next)
+    public function handle(Request $request, \Closure $next): mixed
     {
-        $headers = [
-            'Access-Control-Allow-Origin' => '*',
-            'Access-Control-Allow-Methods' => 'GET, POST, PUT, PATCH, DELETE, OPTIONS',
-            'Access-Control-Allow-Headers' => 'DNT, X-CustomHeader, Keep-Alive, User-Agent, X-Requested-With, If-Modified-Since, Cache-Control, Content-Type, Content-Range, Range, Authorization',
-        ];
-        foreach ($headers as $name => $value) {
-            if (!$request->hasHeader($name)) {
-                $request->header($name, $value);
-            }
-        }
-
-        return $next($request);
+        return $next($request)->header('Access-Control-Allow-Origin', '*');
     }
 }
